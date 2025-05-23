0b956d72fc2213dd1a184ae5e0a8fa7d5fb106cd
Krista Goralczyk <krista.goralczyk@gmail.com>
2025-05-23 11:27:38 -0400

    Add Harper files

---
+++ resources.js
@@ -0,0 +1,11 @@
+// TODO:
+// add endpoint to listen for the following:
+// https://help.twilio.com/articles/31560110671259-How-to-Track-Opt-Out-Opt-In-and-Help-Messages-Using-the-OptOutType-Parameter
+// OptOutType is returned for inbound SMS that contain a keyword i.e. opt in or out
+// ensure individual message & message service
+// store opt out status in db
+// store opt in status in db
+
+// Ideas:
+// filter by PN status with opt-in to send SMS
+// AI to scan inbound for message intent
