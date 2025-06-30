9a04037fd570240c36e0213adf6b7528460c984f
Krista Goralczyk <krista.goralczyk@gmail.com>
2025-06-30 13:35:34 -0400

    tweak comments

--- resources.js
+++ resources.js
@@ -1,12 +1,9 @@
-// keyval optOutType populates on ALL inbound key words: help, start, subscribe, etc.
-// need to track both opt ins and opt outs
 const optOutKeywords = ["cancel", "end", "quit", "stop", "stopall", "unsubscribe", "revoke", "optout"];
 const optInKeywords = ["start", "unstop", "yes"];
 
 function updateStatus(data) {
   const { OptOutType, From } = data;
   const optOut = OptOutType.toLowerCase();
-  // Proceed if its an in/out opt out keyword
   if (optOutKeywords.includes(optOut) || optInKeywords.includes(optOut)) {
     const type = optOutKeywords.includes(optOut) ? 'out' : 'in';
     try {
@@ -20,7 +17,7 @@ function updateStatus(data) {
 
 export class optInStatus extends Resource {
   post (data) {
-    // optOutType is a conditional SMS kv that populates on any keyword, otherwise is null
+    // OptOutType is a conditional kv that populates on a keyword
     if (data.OptOutType) {
       return updateStatus(data);
     }
